ba8f4d095a0ae77d9b587e471a640d69921e9235
Kateryna Dementieva <katerynadementieva@gmail.com>
2025-04-26 13:02:25 +0200

    added paths

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-  return "Hello, Flask!"
+    return "Hello, world"
+
+@app.route('/info')
+def info():
+    return "This is an informational page."
+
+@app.route('/calc/<int:first_int>/<int:second_int>')
+def find_sum(first_int, second_int):
+    sum_int = first_int + second_int
+    return f"The sum of {first_int} and {second_int} is {sum_int}."
+
+@app.route('/reverse/<rev_str>')
+def reverse(rev_str):
+    if len(rev_str) > 0:
+        rev_word = rev_str[::-1]
+        return rev_word
+    return "String must be longer than 0 characters"
+
+@app.route('/user/<name>/<int:age>')
+def greet_age(name, age):
+    if age > 0:
+        return f"Hello, {name}. You are {age} years old."
+    return "Age cannot be negative!"
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
