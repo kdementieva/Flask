eca10359af5cdfc11be4b6360ee5f86493f1e795
Kateryna Dementieva <katerynadementieva@gmail.com>
2025-04-28 20:37:57 +0200

    funny-form

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,5 @@
 from app import app
-from flask import render_template
-
-@app.route('/hello')
-def hello():
-    return "Hello, world"
-
-@app.route('/info')
-def info():
-    return "This is an informational page."
-
-@app.route('/calc/<int:first_int>/<int:second_int>')
-def find_sum(first_int, second_int):
-    sum_int = first_int + second_int
-    return f"The sum of {first_int} and {second_int} is {sum_int}."
-
-@app.route('/reverse/<rev_str>')
-def reverse(rev_str):
-    if len(rev_str) > 0:
-        rev_word = rev_str[::-1]
-        return rev_word
-    return "String must be longer than 0 characters"
-
-@app.route('/user/<name>/<int:age>')
-def greet_age(name, age):
-    if age > 0:
-        return f"Hello, {name}. You are {age} years old."
-    return "Age cannot be negative!"
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+from flask import render_template, request, redirect, url_for
 
 @app.route("/about")
 def about():
@@ -38,3 +8,20 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        color = request.form.get("color")
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies") 
+        level = request.form.get("level")
+        return render_template("result.html", name=name, email=email, color=color, profession=profession, hobbies=hobbies, level=level)
+    else:
+        return redirect(url_for("form"))
